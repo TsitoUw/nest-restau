9cf292119b9c49b07bf0c459e85f2502cebcb2fa
Princy Rasolonjatovo <princy.m.rasolonjatovo@gmail.com>
2023-07-03 09:28:09 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true});
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule, { cors: true });
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true,
+			transformOptions: {
+				enableImplicitConversion: true,
+			},
+		}),
+	);
+
+
+	const options = new DocumentBuilder()
+		.setTitle('restaurant')
+		.setDescription('A restaurant back office')
+		.setVersion('0.1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(3000);
 }
 bootstrap();
